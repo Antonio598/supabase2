ee603f8958cc665999d4c65dc9005ef604865932
Antonio598 <israelmayalara@gmail.com>
2026-03-11 22:58:55 -0600

    Create fix_passwords.sql

---
+++ docker/volumes/db/fix_passwords.sql
@@ -0,0 +1,4 @@
+-- Este script corre al final para asegurar que las claves sean las tuyas
+ALTER ROLE authenticator WITH PASSWORD 'Synexia_Secure_Pass_2026!';
+ALTER ROLE postgres WITH PASSWORD 'Synexia_Secure_Pass_2026!';
+ALTER ROLE supabase_admin WITH PASSWORD 'Synexia_Secure_Pass_2026!';
