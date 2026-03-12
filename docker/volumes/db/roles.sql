e1ebeccf6e808b058ed838da00d4aaff13b0ba94
Antonio598 <israelmayalara@gmail.com>
2026-03-11 22:53:41 -0600

    Update roles.sql

--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -1,48 +1,11 @@
--- 1. Creamos los roles base de Supabase si no existen
+-- Forzamos la creación de los roles base
 DO $$ 
 BEGIN
   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'authenticator') THEN
     CREATE ROLE authenticator NOINHERIT LOGIN PASSWORD 'Synexia_Secure_Pass_2026!';
   END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'anon') THEN
-    CREATE ROLE anon NOLOGIN;
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'authenticated') THEN
-    CREATE ROLE authenticated NOLOGIN;
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'service_role') THEN
-    CREATE ROLE service_role NOLOGIN;
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'supabase_auth_admin') THEN
-    CREATE ROLE supabase_auth_admin NOLOGIN;
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'supabase_storage_admin') THEN
-    CREATE ROLE supabase_storage_admin NOLOGIN;
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'supabase_functions_admin') THEN
-    CREATE ROLE supabase_functions_admin NOLOGIN;
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'pgbouncer') THEN
-    CREATE ROLE pgbouncer NOLOGIN;
-  END IF;
+  -- Repite para los demás si es necesario
 END $$;
 
--- 2. Ahora que sabemos que existen, actualizamos sus contraseñas
--- Usamos la misma que tienes en tus variables de entorno de Easypanel
+-- Ahora sí cambiamos contraseñas
 ALTER USER authenticator WITH PASSWORD 'Synexia_Secure_Pass_2026!';
-ALTER USER supabase_auth_admin WITH PASSWORD 'Synexia_Secure_Pass_2026!';
-ALTER USER supabase_functions_admin WITH PASSWORD 'Synexia_Secure_Pass_2026!';
-ALTER USER supabase_storage_admin WITH PASSWORD 'Synexia_Secure_Pass_2026!';
-ALTER USER pgbouncer WITH PASSWORD 'Synexia_Secure_Pass_2026!';
-
--- 3. Asignamos los permisos de membresía básicos
-GRANT anon TO authenticator;
-GRANT authenticated TO authenticator;
-GRANT service_role TO authenticator;
